f5bc54e531e78112bc1315b778a1a06e81ab96c0
scarf <greenscarf005@gmail.com>
2022-05-24 19:44:30 +0900

    chore: temporary test program and config
    maybe i could parse this?

---
+++ src/libutil/util.hpp
@@ -0,0 +1,10 @@
+#ifndef UTIL_UTIL_HPP
+#define UTIL_UTIL_HPP
+
+namespace util {
+
+
+}  // namespace util
+
+
+#endif // UTIL_UTIL_HPP
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,32 @@
+#include <fstream>
 #include <iostream>
+#include <libstrutil/strutil.hpp>
+#include <libutil/types.hpp>
 #include <main/main.hpp>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+
+void erase_comments(string& line,const string& comment = "#" ) {
+  string::size_type comment_index;
+
+  if ((comment_index = line.find(comment)) != string::npos)
+    line.erase(comment_index);
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::ifstream infile("/Users/youkim/Repo/webserv/config.conf");
+  string line;
+  for (i32 i = 0; std::getline(infile, line); i++) {
+    erase_comments(line);
+
+    vector<string> tokens = strutil::split(line);
+    std::cout << "line " << i << std::endl;
+    strutil::print_vector(tokens);
+  }
+
   return 0;
 }
