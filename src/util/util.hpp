daf49616e7e7acb46ce8a0d5ebd2304173fdb1be
scarf <greenscarf005@gmail.com>
2022-05-26 18:09:32 +0900

    chore: 안 쓰는 파일 삭제

--- src/util/util.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_UTIL_HPP
-#define UTIL_UTIL_HPP
-
-#define COMPTIME_ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-#define COMPTIME_ARRAY_END(a) a + COMPTIME_ARRAY_SIZE(a)
-
-namespace util {}  // namespace util
-
-#endif  // UTIL_UTIL_HPP
