3a3dbf52a1fe2fe694c319259e876b9a74452c7a
scarf <greenscarf005@gmail.com>
2022-05-30 16:40:51 +0900

    refactor: parser 라이브러리에 정리

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,99 +1,19 @@
-#include <exception>
-#include <fstream>
 #include <iostream>
-#include <logger/logger.hpp>
-#include <logger/message.hpp>
-#include <map>
+#include <parser/tokenize.hpp>
 #include <prettyprint/prettyprint.hpp>
-#include <queue>
 #include <string>
-#include <utility>
 #include <vector>
 
-using std::map;
 using std::pair;
-using std::queue;
 using std::string;
 using std::vector;
 
-// struct Tokens {
-//   private:
-
-//    public:
-
-// };
-
-// class tokenizer {
-//   private:
-//   public:
-// };
-// struct tokentype_t {
-//   enum e { BracketBegin, BracketEnd };
-// };
-
-// struct token {
-//   string type;
-//   // tokentype_t::e type;
-//   string value;
-// };
-
-// map<string, tokentype_t::> init_token_types() {
-//   map<string, string> token_types;
-//   token_types["{"] = "BracketBegin";
-//   token_types["}"] = "BracketEnd";
-//   token_types["server"] = "Server";
-//   token_types["port"] = "Port";
-//   token_types["server_names"] = "ServerNames";
-//   token_types["username"] = "Username";
-//   token_types["password"] = "Password";
-
-//   return token_types;
-// }
-
-// string get_token_type(const string& s) {
-//   static map<string, string> token_types = init_token_types();
-
-//   return token_types[s];
-// }
-typedef vector<string> tokens_t;
-
-tokens_t get_tokens(std::ifstream& infile) {
-  using namespace util;
-
-  string line;
-  tokens_t tokens;
-
-  for (int i = 0; std::getline(infile, line); i++) {
-    util::erase_from(line, "#");
-    if (line.empty())
-      continue;
-
-    vector<string> t = util::split(line);
-
-    tokens.reserve(tokens.size() + t.size() + 1);
-    for (vector<string>::iterator it = t.begin(); it != t.end(); ++it)
-      tokens.push_back(*it);
-    tokens.push_back("\n");
-  }
-
-  return tokens;
-}
-
 int main() {
+  using util::tokens_t;
   std::ifstream infile("config.conf");
 
-  tokens_t tokens = get_tokens(infile);
+  tokens_t tokens = util::tokenize(infile);
   util::as_table(tokens);
-
-  // std::ifstream infile("/Users/youkim/Repo/webserv/config.conf");
-  // string line;
-  // for (int i = 0; std::getline(infile, line); i++) {
-  //   util::erase_from(line, "#");
-
-  //   vector<string> tokens = util::split(line);
-  //   std::cout << "line " << i << std::endl;
-  //   util::print_vector(tokens);
-  // }
 
   return 0;
 }
---
+++ src/parser/tokenize.tpp
@@ -0,0 +1,9 @@
+#ifndef PARSER_TOKENIZE_TPP
+#define PARSER_TOKENIZE_TPP
+
+#include <parser/tokenize.hpp>
+
+namespace util {
+}  // namespace util
+
+#endif // PARSER_TOKENIZE_TPP
